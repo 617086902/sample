e10e42038d81062c9bae6cfe44dd3e7b5e257801
gege <617086902@qq.com>
2018-01-06 02:33:22 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <title>@yield('title','sample')</title>
+    <link rel="stylesheet" href="/css/abc.css">
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
-    @yield('content')
+    @include('layouts/_header')
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
